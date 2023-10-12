deacbbd6fea77b83fd5d462ec26ab0a42883c5cb
12091990 <kumbarvasanth@gmail.com>
2023-10-12 14:52:11 +0000

    adding bucket new

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-vasanth1238721367"
   acl    = "private"
 
   versioning = {
