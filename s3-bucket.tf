0bad7513dd21759de5af3e108e035d034afe9e70
12091990 <kumbarvasanth@gmail.com>
2023-10-12 19:44:57 +0000

    Added s3 bucket module123567

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "vasanth56789087"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
+module "s3-bucket" {
+  source  = "app.terraform.io/terraformambassador/s3-bucket/aws"
+  version = "2.8.0"
 }
-
